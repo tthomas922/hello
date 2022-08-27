2f384a7cfbe1611e56f6d5d94b4ec6729991e6b7
tthomas922 <Tyler>
2022-08-27 19:24:08 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,1 +1,18 @@
-git remote add origin https://LOGIN:TOKEN@github.com/LOGIN/hello.git
+/**
+*Tyler Thomas
+*tthomas32@unomaha.edu
+*2022/08/27
+*
+*My very first hack!
+*
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+printf("Computer Engineering is my major, I'm Tyler Thomas!\n");
+
+return 0;
+}
